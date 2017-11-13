564abdcc5a414c2609a94eae63c6dd9f9f96d7fd
caezar47 <iss47@ya.ru>
2017-11-13 08:55:49 +0300

    fix pages

--- temp/tpl/blog/index.tpl.php
+++ temp/tpl/blog/index.tpl.php
@@ -1,4 +1,6 @@
 <?
+$bg_prefix="bg-block__";
+$block_prefix="blog-page__";
 $posts = $this->getItems(array(
 	'post_type' => 'post',
 	'posts_per_page' => 10,
@@ -19,32 +21,63 @@ $posts = $this->getItems(array(
 $heading_small = get_field('page_heading_small', $id);
 $heading_color = get_field('heading_color', $id);
 ?>
+<div class="content-block <?=$block_prefix;?>content-block  <?=$bg_prefix;?>block" role="main">	
+	<div class="container content-block__container <?=$block_prefix;?>container <?=$bg_prefix;?>container">
+		<?
+		// хлебные крошки первого уровня
+		$this->tpl(
+			'_/bread', 
+			array(
+				"block_prefix" => "breadcrumb__",
+				"block_mod" => "is--heading  is--center"
+			)
+		);
+		// заголовок страницы		
+		$this->tpl(
+			'_/heading', 
+			array(
+				"block_prefix" => "page-header__",
+				"block_mod" => "is--heading  is--center"
+			)
+		);
+		?>
+		<div class="blog-panel__block">
+			<?	
+			// навигация по разделам		
+			$this->tpl(
+				'blog/navbar', 
+				array(
+					"block_prefix" => "navbar-category__",
+					"block_mod" => "is--nofixed"
+				)
+			);
+			?>
+		</div>
+	</div>
+	<?
+		//фоновые картинки
+		$this->tpl(
+			'_/bg', 
+			array(
+				"block_prefix" => "bg-block__",
+				"block_mod" => "is--branch",
+				"block_bg" => "bg-branch.png",
+			)
+		);
+		$this->tpl(
+			'_/bg', 
+			array(
+				"block_prefix" => "bg-block__",
+				"block_mod" => "is--candle",
+				"block_bg" => "bg-candle.png",
+			)
+		);
+	?>
+</div>
+
 <div class="content-block blog-page__content-block  is--hidden" role="main">	
 	<div class="page-header-panel__block  <?=$heading_color;?>">
-		<div class="page-header-panel__container container">
-			<div class="breadcrumb__block  is--heading  is--blue">
-				<ul class="breadcrumb__list  is--heading  is--blue">
-					<li class="breadcrumb__list-item">
-						<a href="/" class="breadcrumb__list-link">Главная</a>
-					</li>
-					<li class="breadcrumb__list-item is--active"><?=t($id);?></li>
-				</ul>
-			</div>	
-			<div class="page-header__block  is--heading  is--blue  is--panel">
-				<h1 class="page-header__heading  is--heading  is--blue  is--panel"><span><?=t($id);?></span></h1>	
-				<?if ($heading_small != ""){?>
-				<h3 class="page-header__heading-small  is--heading  is--blue  is--panel"><?=$heading_small;?></h3>		
-				<?}?>
-			</div>
-			<?
-				$this->tpl(
-					'_/bg-round', 
-					array(
-						"block_prefix"=>"bg-round__",
-					)
-				);
-			?> 	
-		</div>	 
+		 
 		<?
 		$this->tpl(
 			'_/bg-card', 
--- temp/tpl/blog/item.tpl.php
+++ temp/tpl/blog/item.tpl.php
@@ -1,58 +1,86 @@
-<div class="content-block blog-page__content-block  is--hidden" role="main">	
-	<div class="page-header-panel__block  <?=$heading_color;?>">
-		<div class="page-header-panel__container container">
-			<div class="breadcrumb__block  is--heading  is--blue  is--center">
-				<ul class="breadcrumb__list  is--heading  is--blue  is--center">
-					<li class="breadcrumb__list-item">
-						<a href="/" class="breadcrumb__list-link">Главная</a>
-					</li>
-					<li class="breadcrumb__list-item">
-						<a href="<?=l(4);?>" class="breadcrumb__list-link"><?=t(4);?></a>
-					</li>
-					<li class="breadcrumb__list-item is--active"><?=t($id);?></li>
-				</ul>
-			</div>	
-			<div class="page-header__block  is--heading  is--blue  is--panel  is--center">
-				<h1 class="page-header__heading  is--heading  is--blue  is--panel  is--center"><span><?=t($id);?></span></h1>	
-				<?if ($heading_small != ""){?>
-				<h3 class="page-header__heading-small  is--heading  is--blue  is--panel"><?=$heading_small;?></h3>		
-				<?}?>
-			</div>
-			<?
-				$this->tpl(
-					'_/bg-round', 
-					array(
-						"block_prefix"=>"bg-round__",
-					)
-				);
-			?> 	
-		</div>	 
-		<?
-		$this->tpl(
-			'_/bg-card', 
-			array(
-				"block_prefix" => "bg-plane__",
-				"block_mod" => "is--clients-left  is--speed-25",
-				"block_bg" => "bg-plane-clients-white.png",
-			)
-		);
-		?>
-		<?
-		$this->tpl(
-			'_/bg-card', 
-			array(
-				"block_prefix" => "bg-plane__",
-				"block_mod" => "is--clients-right  is--speed-10",
-				"block_bg" => "bg-plane-clients-blue.png",
-			)
-		);
-		?>	
-	</div>
-	<div class="container content-block__container blog-page__container  bg-header__container">
+<?
+$bg_prefix="bg-block__";
+$block_prefix="blog-page__";
+$breadcrumb_prefix = "breadcrumb__";
+$breadcrumb_mod="is--heading  is--center";
+$heading_prefix="page-header__";
+$heading_mod="is--heading  is--center";
+?>
+<div class="content-block <?=$block_prefix;?>content-block  <?=$bg_prefix;?>block" role="main">	
+	<div class="container content-block__container <?=$block_prefix;?>container <?=$bg_prefix;?>container">
+		<div class="<?=$breadcrumb_prefix;?>block  <?=$breadcrumb_mod;?>">
+			<ul class="<?=$breadcrumb_prefix;?>list  <?=$breadcrumb_mod;?>">
+				<li class="<?=$breadcrumb_prefix;?>list-item">
+					<a href="<?=l(1);?>" class="<?=$breadcrumb_prefix;?>list-link">Главная</a>
+				</li>
+				<li class="<?=$breadcrumb_prefix;?>list-item">
+					<a href="<?=l(4);?>" class="<?=$breadcrumb_prefix;?>list-link"><?=t(4);?></a>
+				</li>
+				<?
+				$categories = get_the_category($id);				
+				if(count($categories)) {
+					foreach($categories as $cat) {
+						$link = get_category_link($cat->term_id);
+				?>					
+					<li class="<?=$breadcrumb_prefix;?>list-item"><a href="<?=$link;?>" class="<?=$breadcrumb_prefix;?>list-link"><?=$cat->name;?></a></li>
+				<?
+					}
+				}
+				?>
+				<li class="<?=$breadcrumb_prefix;?>list-item is--active"><?=t();?></li>
+			</ul>
+		</div>
+		<div class="<?=$heading_prefix;?>block  <?=$heading_mod;?>">
+			<h1 class="<?=$heading_prefix;?>heading  <?=$heading_mod;?>"><span><?=t();?></span></h1>
+		</div>
+
 		<div class="text__container">
 			<div class="text__block">
 				<?=c($this->post['id']);?>
 			</div>
 		</div>
 	</div>
+	<?
+		//фоновые картинки
+		$this->tpl(
+			'_/bg', 
+			array(
+				"block_prefix" => "bg-block__",
+				"block_mod" => "is--leaf-xl  is--blog-item",
+				"block_bg" => "bg-leaf-xl.png",
+			)
+		);
+		$this->tpl(
+			'_/bg', 
+			array(
+				"block_prefix" => "bg-block__",
+				"block_mod" => "is--syringes-insulin  is--blog-item",
+				"block_bg" => "bg-syringes-insulin.png",
+			)
+		);
+		$this->tpl(
+			'_/bg', 
+			array(
+				"block_prefix" => "bg-block__",
+				"block_mod" => "is--cream-leaf  is--blog-item",
+				"block_bg" => "bg-cream-leaf.png",
+			)
+		);
+		$this->tpl(
+			'_/bg', 
+			array(
+				"block_prefix" => "bg-block__",
+				"block_mod" => "is--cream-tube  is--blog-item",
+				"block_bg" => "bg-cream-tube.png",
+			)
+		);
+		$this->tpl(
+			'_/bg', 
+			array(
+				"block_prefix" => "bg-block__",
+				"block_mod" => "is--flover-white  is--blog-item",
+				"block_bg" => "bg-flover-white.png",
+			)
+		);
+	?>
 </div>
