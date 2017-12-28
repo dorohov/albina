76e97fa58998d542312c22e715f509f79b2ce2f0
caezar47 <iss47@ya.ru>
2017-12-28 09:59:46 +0300

    fix

--- src/js/slick.jquery/slick.ready.js
+++ src/js/slick.jquery/slick.ready.js
@@ -5,15 +5,29 @@ $(function() {
 		slidesToScroll: 1,
 		arrows: true,
 		draggable: false,
-		swipe: false,
+		//swipe: false,
 		//fade: true,
 		asNavFor: '#slick-nav',
 		variableWidth: true,
 		centerMode: true,
 		prevArrow: '<button type="button" class="slick-prev"><svg class="icon-svg icon-owl-prev" role="img"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/wp-content/themes/azbn7theme/img/svg/sprite.svg#owl-prev"></use></svg></button>',
-		nextArrow: '<button type="button" class="slick-next"><svg class="icon-svg icon-owl-next" role="img"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/wp-content/themes/azbn7theme/img/svg/sprite.svg#owl-next"></use></svg></button>'
+		nextArrow: '<button type="button" class="slick-next"><svg class="icon-svg icon-owl-next" role="img"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/wp-content/themes/azbn7theme/img/svg/sprite.svg#owl-next"></use></svg></button>',
 		//prevArrow: '<button type="button" class="slick-prev"><svg class="icon-svg icon-owl-prev" role="img"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/img/svg/sprite.svg#owl-prev"></use></svg></button>',
 		//nextArrow: '<button type="button" class="slick-next"><svg class="icon-svg icon-owl-next" role="img"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/img/svg/sprite.svg#owl-next"></use></svg></button>'
+		responsive: [
+		    {
+		      breakpoint: 769,
+		      settings: {
+				swipe: false,
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+				swipe: true,
+		      }
+		    }
+	  	]
 	});
 	$('#slick-nav').slick({
 		slidesToShow: 5,
@@ -23,18 +37,20 @@ $(function() {
 		centerMode: true,
 		focusOnSelect: true,
 		draggable: false,
-		swipe: false,
+		//swipe: false,
 		responsive: [
 		    {
 		      breakpoint: 769,
 		      settings: {
-		        slidesToShow: 4
+		        slidesToShow: 4,
+				swipe: false,
 		      }
 		    },
 		    {
 		      breakpoint: 480,
 		      settings: {
-		        slidesToShow: 1
+		        slidesToShow: 1,
+				swipe: true,
 		      }
 		    }
 	  	]
